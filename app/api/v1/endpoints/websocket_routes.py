5547207d9b1d6f3c2f8f7afd5a87277ae3862ad2
cheer <dehai.zhao@anu.edu.au>
2023-11-17 23:16:08 +1100

    change env

--- app/api/v1/endpoints/websocket_routes.py
+++ app/api/v1/endpoints/websocket_routes.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, WebSocket
 from ....services.chat_service import ChatService
+from ....core.config import settings
 
 router = APIRouter()
 
-@router.websocket("/ws/chat")
+@router.websocket(settings.WEBSOCKET_ROUTE)
 async def chat_websocket(websocket: WebSocket):
     chat_service = ChatService()
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
         async for part in chat_service.process_message(data):
-            print(part)
             await websocket.send_text(part)
