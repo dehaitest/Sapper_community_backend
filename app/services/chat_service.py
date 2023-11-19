828de336ade330bda5d642147b95aa7f0ea8a5d0
cheer <dehai.zhao@anu.edu.au>
2023-11-19 14:15:35 +1100

    upgrade dependencies

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -11,11 +11,10 @@ class ChatService:
         self.chatbot.chat_history = self.chatbot.chat_history + message
         history = ''
         async for part in self.chatbot.chat():
-            if part.choices[0]["finish_reason"] == "stop":
+            if part.choices[0].finish_reason == "stop":
                 self.chatbot.chat_history.append({"role": "assistant", "content": history})
                 yield "__END_OF_RESPONSE__"  # Special marker indicating end of response
                 break
-            history += part['choices'][0]['delta']['content']
+            history += part.choices[0].delta.content
             await asyncio.sleep(0.001)
-            yield part['choices'][0]['delta']['content']
-
+            yield part.choices[0].delta.content
