a47af63393eb0d1ebc4755ef3cdd7927018f1e94
YuCheng1106 <3129577193@qq.com>
2023-11-25 23:18:05 +0800

    add workspace routes

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,9 @@
 # app/api/v1/api.py
 from fastapi import APIRouter
-from .endpoints import user_routes, websocket_routes
+from .endpoints import user_routes, websocket_routes, workspace_routes
 
 api_router = APIRouter()
 api_router.include_router(user_routes.router, tags=["users"])
 api_router.include_router(websocket_routes.router, tags=["chatbot"])
+api_router.include_router(websocket_routes.router, tags=["workspace"])
 # Include other routers as needed
