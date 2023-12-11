f4159c438dd1c865beb81a599270ad62e2a07c5a
cheer <dehai.zhao@anu.edu.au>
2023-12-11 11:25:52 +1100

    revise database pool size

--- app/services/database.py
+++ app/services/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from ..core.config import settings  # Import your settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
 )
